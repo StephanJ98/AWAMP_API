b583e43f3b2289d00eb12681295a92fac2029f71
StephanJ98 <diego.jeandon@gmail.com>
2023-08-07 14:38:27 +0200

    :construction: Add MongoDB driver & connect function

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 import express from 'express'
+import cors from 'cors'
 import 'dotenv/config'
-const app = express()
-const port = process.env.PORT | 3000
+import { MongoClient, ObjectId } from 'mongodb'
 
+// Otros paquetes de rutas
 import UserRouter from './routes/User.js'
 
+const app = express()
+const port = process.env.PORT | 4000
+
+let cacheDB = null
+
+const connectToDatabase = async (uri) => {
+    if (cacheDB) return cacheDB
+
+    const client = await MongoClient.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    const db = await client.db(new URL(uri).pathname.substring(1))
+    cacheDB = db
+    return db
+}
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// Importación de otras subrutas especificas
 app.use('/user', UserRouter)
 
 app.get('/', (_req, res) => {
